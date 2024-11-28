fd1c56f779b87e740a6d63e5e924a6c125d34a15
GeorgeW-alt2 <gw67656@gmail.com>
2024-11-28 22:53:22 +1100

    done!

--- interpreter.py
+++ interpreter.py
@@ -6,7 +6,7 @@ ser = serial.Serial('COM3', 9600, timeout=1)
 line = ""
 try:
     print("Please wait for the numbers to be generated (Ctrl+C to exit).")
-    for i in range(100*2): #random numbers to generate
+    for i in range(200*2): #random numbers to generate
         time.sleep(0.05) # sleep for stability
         line += ser.read().decode('utf-8').strip()  # Read a line from the serial port
     print(line)  # Display the number
