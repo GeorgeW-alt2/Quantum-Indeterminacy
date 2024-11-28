262a6411cf09815edfdabb5a22a7cd5acc858622
GeorgeW-alt2 <gw67656@gmail.com>
2024-11-28 22:09:13 +1100

    Add files via upload

--- interpreter.py
+++ interpreter.py
@@ -1,15 +1,15 @@
 import serial
-
+import time
 # Configure the serial connection
 # Replace 'COM3' with the port name your Arduino is connected to
 ser = serial.Serial('COM3', 9600, timeout=1)
 line = ""
 try:
-    print("Press Enter to display the next number (Ctrl+C to exit).")
-    while True:
-        #input("Press Enter: ")  # Wait for user input
+    print("Please wait for the numbers to be generated (Ctrl+C to exit).")
+    for i in range(100*2): #random numbers to generate
+        time.sleep(0.05) # sleep for stability
         line += ser.read().decode('utf-8').strip()  # Read a line from the serial port
-        print(line)  # Display the number
+    print(line)  # Display the number
 except KeyboardInterrupt:
     print("\nExiting...")
 finally:
